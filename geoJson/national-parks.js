d8cad617523311002d5e7a6f52d4376a4725da15
taylor douthit <taylordouthit4@gmail.com>
2024-02-02 09:44:37 -0600

    updated package json meta, reorganized indexjs, added comments for clarity

--- geoJson/national-parks.js
+++ geoJson/national-parks.js
@@ -1,4 +1,4 @@
-export const nationalParks = {
+export const nationalParksData = {
   type: "FeatureCollection",
   features: [
     {
--- index.js
+++ index.js
@@ -1,17 +1,39 @@
-import { statesData } from "./us-states.js";
-import { nationalParks } from "./nps-data.js";
+import { statesData } from "./geoJson/us-states.js";
+import { nationalParksData } from "./geoJson/national-parks.js";
 import { parkIcon } from "./components/park-icon.js";
 import { legend } from "./components/legend.js";
+import { info } from "./components/info.js";
 
-// Map: used to create a map on a page and manipulate it.
+// Create a map
 const map = L.map("map").setView([43, -114], 4);
 
-// OpenStreetMap: https://www.openstreetmap.org/about
+// Adds roads, lakes, topography, etc.
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 8,
   minZoom: 3,
   attribution:
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+
+// Gives background color to states and defines the mousein/out/hover.
+let geojson = L.geoJson(statesData, {
+  style: style,
+  onEachFeature: onEachFeature,
+}).addTo(map);
+
+// Custom Info Control
+info.addTo(map);
+
+// Custom Legend Control
+legend.addTo(map);
+
+// Given national parks data, adds a marker at the latlng of each park.
+L.geoJSON(nationalParksData, {
+  pointToLayer: function (feature, latlng) {
+    return L.marker(latlng, { icon: parkIcon }).bindPopup(
+      feature.properties.name
+    );
+  },
 }).addTo(map);
 
 // Adding Some Color
@@ -33,22 +55,8 @@ export function getColor(d) {
     : "#FFEDA0";
 }
 
-function style(feature) {
-  return {
-    fillColor: getColor(feature.properties.density),
-    weight: 2,
-    opacity: 1,
-    color: "white",
-    dashArray: "3",
-    fillOpacity: 0.7,
-  };
-}
-
-let geojson = L.geoJson(statesData, { style }).addTo(map);
-
-// Adding Interaction
+// Highlights feature
 function highlightFeature(e) {
-  // access to the layer that was hovered through e.target
   var layer = e.target;
 
   layer.setStyle({
@@ -64,14 +72,26 @@ function highlightFeature(e) {
   info.update(layer.feature.properties);
 }
 
-// On mouseout
+// Styles features
+function style(feature) {
+  return {
+    fillColor: getColor(feature.properties.density),
+    weight: 2,
+    opacity: 1,
+    color: "white",
+    dashArray: "3",
+    fillOpacity: 0.7,
+  };
+}
+
+// Resets highlights on features
 function resetHighlight(e) {
   geojson.resetStyle(e.target);
 
   info.update();
 }
 
-// zooms to the state
+// Zooms to feature
 function zoomToFeature(e) {
   map.fitBounds(e.target.getBounds());
 }
@@ -84,44 +104,3 @@ function onEachFeature(feature, layer) {
     click: zoomToFeature,
   });
 }
-
-geojson = L.geoJson(statesData, {
-  style: style,
-  onEachFeature: onEachFeature,
-}).addTo(map);
-
-// Custom Info Control
-var info = L.control();
-
-info.onAdd = function (map) {
-  this._div = L.DomUtil.create("div", "info"); // create a div with a class "info"
-  this.update();
-  return this._div;
-};
-
-// method that we will use to update the control based on feature properties passed
-info.update = function (props) {
-  this._div.innerHTML =
-    "<h4>US Population Density</h4>" +
-    (props
-      ? "<b>" +
-        props.name +
-        "</b><br />" +
-        props.density +
-        " people / mi<sup>2</sup>"
-      : "Hover over a state");
-};
-
-info.addTo(map);
-
-// Custom Legend Control
-legend.addTo(map);
-
-// Create a GeoJSON layer with custom icons
-L.geoJSON(nationalParks, {
-  pointToLayer: function (feature, latlng) {
-    return L.marker(latlng, { icon: parkIcon }).bindPopup(
-      feature.properties.name
-    );
-  },
-}).addTo(map);
